724666dec6a4e08ec97deefbc7e2834e026e0e1c
FundaErmete <funda.ermete@gmail.com>
2024-01-09 13:10:14 +0100

    Add an object

--- Exercise1/index.php
+++ Exercise1/index.php
@@ -2,38 +2,45 @@
 
 declare(strict_types=1);
 
-class Beverage =
+class Beverage 
 {
     // Properties
     public string $color;
     public float $price;
     public string $temperature;
-}
 
     // Constructor with default value for temperature
     public function __construct(string $color, float $price, string $temperature = "cold") {
         $this->color = $color;
         $this->price = $price;
         $this->temperature = $temperature;
-    };
+    }
 
-
-     // Functions
-     public function getColor(): string {
+    // Functions
+    public function getColor(): string {
         return $this->color;
-     }
+    }
 
-     public function getPrice(): float {
+    public function getPrice(): float {
         return $this->price;
-     }
+    }
 
-     public function getTemparature(): string {
+    public function getTemperature(): string {
         return $this->temperature;
-     }
+    }
 
-     public function getInfo(): string {
+    public function getInfo(): string {
         return "This beverage is {$this->temperature} and {$this->color}.";
-     }
-    
+    }
+}
+
+// Instantiate an object representing cola
+$cola = new Beverage("Black", 2.0);
+
+// Print the result of getInfo on the screen
+echo $cola->getInfo() . "\n";
+
+// Print the temperature on the screen
+echo "Temperature: " . $cola->getTemperature() . "\n";
 
 ?>
